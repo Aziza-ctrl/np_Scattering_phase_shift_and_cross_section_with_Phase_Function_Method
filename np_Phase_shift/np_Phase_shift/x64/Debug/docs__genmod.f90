2299278e9b94ad8dacfe6ef60276a4871798b940
Aziza-ctrl <jhonperdomo902@gmail.com>
2025-12-06 15:34:11 -0500

    Add files via upload

---
+++ np_Phase_shift/np_Phase_shift/x64/Debug/docs__genmod.f90
@@ -0,0 +1,10 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Tue Sep  2 16:30:48 2025
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE DOCS__genmod
+          INTERFACE 
+            SUBROUTINE DOCS(UNI)
+              INTEGER(KIND=4) :: UNI
+            END SUBROUTINE DOCS
+          END INTERFACE 
+        END MODULE DOCS__genmod
